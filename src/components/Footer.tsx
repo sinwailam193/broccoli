7589c005919533348b02188b0bc64ae472abf378
Aaron <8454621+sinwailam193@users.noreply.github.com>
2024-08-23 20:19:18 +0800

    Center footer text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
         <footer aria-labelledby="footer-heading" className="bg-gray-900">
             <div className="mx-auto max-w-7xl px-6 pb-8 lg:px-8">
                 <div className="border-t border-white/10 pt-8">
-                    <p className="text-xs leading-5 text-gray-400">
+                    <p className="text-xs leading-5 text-gray-400 text-center">
                         &copy; {new Date().getFullYear()} Broccoli & Co. All
                         rights reserved.
                     </p>
